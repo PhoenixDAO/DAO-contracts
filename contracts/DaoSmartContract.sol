a719ad83978ad558814407083830d8f8836362a4
Danial Ansari <danialansari998@gmail.com>
2021-05-19 14:44:25 +0500

    Fixing Coding Conventions

--- contracts/DaoSmartContract.sol
+++ contracts/DaoSmartContract.sol
@@ -9,12 +9,7 @@ contract DaoSmartContract is OwnableUpgradeSafe, PausableUpgradeSafe {
     using SafeMath for uint256;
 
     enum Status {PENDING, UPVOTE, VOTING, ACTIVE, COMPLETED, REJECTED}
-    // 0 -> pending
-    // 1 -> upvote
-    // 2 -> voting
-    // 3 -> active
-    // 4 -> completed
-    // 5 -> rejected
+
     address public phnxContractAddress;
     
     uint256 public collateralAmount;
@@ -34,7 +29,7 @@ contract DaoSmartContract is OwnableUpgradeSafe, PausableUpgradeSafe {
     }
 
     event ProposalSubmitted(
-        string _Id,
+        string  proposalId,
         uint256 fundsRequested,
         uint256 initiationTimestamp,
         uint256 completionTimestamp,
@@ -46,7 +41,7 @@ contract DaoSmartContract is OwnableUpgradeSafe, PausableUpgradeSafe {
     );
 
     event ProposalEditted(
-        string _Id,
+        string  proposalId,
         uint256 fundsRequested,
         uint256 initiationTimestamp,
         uint256 completionTimestamp,
@@ -57,31 +52,23 @@ contract DaoSmartContract is OwnableUpgradeSafe, PausableUpgradeSafe {
         address proposer
     );
     event ProposalStatusUpdated(
-        string _proposalId,
-        uint256 _previousStatus,
-        uint256 _newStatus
+        string  proposalId,
+        uint256  previousStatus,
+        uint256  newStatus
     );
 
     event ColleteralWithdrawn(
-        string _proposalId,
-        uint256 _collateralAmount,
-        address _proposer
+        string proposalId,
+        uint256 collateralAmount,
+        address proposer
     );
-    // event Voted(
-    //     string _proposalId,
-    //     uint256 altQuantity,
-    //     uint256 _days,
-    //     uint256 rewardAmount
-    // );
+
   
 
     function initialize(address _phoenixContractAddress) external initializer {
         OwnableUpgradeSafe.__Ownable_init();
         PausableUpgradeSafe.__Pausable_init();
-        // phnxContractAddress = 0xfe1b6ABc39E46cEc54d275efB4b29B33be176c2A;
         phnxContractAddress = _phoenixContractAddress;
-        // phnxStakingContractAddress = 0xe5242650Ed4a1C0bBa33204Efa1ca76772a5544C;
-        
     }
 
     function updateProposal(
@@ -89,32 +76,32 @@ contract DaoSmartContract is OwnableUpgradeSafe, PausableUpgradeSafe {
         uint256 endTimestamp,
         uint256 colletralAmount,
         uint256 totalMilestones,
-        string calldata _proposalId
+        string calldata proposalId
     ) external whenNotPaused {
-        require(proposalList[_proposalId].proposer != address(0),"This proposal Does not exist");
+        require(proposalList[proposalId].proposer != address(0),"This proposal Does not exist");
         address sender = msg.sender;
-        require(proposalList[_proposalId].proposer == sender,"Only Owner of propsal can edit this propsal");
-        proposalList[_proposalId] = Proposal(
+        require(proposalList[proposalId].proposer == sender,"Only Owner of propsal can edit this propsal");
+        proposalList[proposalId] = Proposal(
             fundsRequested,
             0,
             endTimestamp,
             colletralAmount,
             totalMilestones,
             0,
-            0, //status
+            0, 
             0,
             sender
         );
         emit ProposalEditted(
-            _proposalId,
-            proposalList[_proposalId].fundsRequested,
-            proposalList[_proposalId].initiationTimestamp,
-            proposalList[_proposalId].completionTimestamp,
-            proposalList[_proposalId].colletralAmount,
-            proposalList[_proposalId].totalMilestones,
-            proposalList[_proposalId].status,
-            proposalList[_proposalId].totalVotes,
-            proposalList[_proposalId].proposer
+            proposalId,
+            proposalList[proposalId].fundsRequested,
+            proposalList[proposalId].initiationTimestamp,
+            proposalList[proposalId].completionTimestamp,
+            proposalList[proposalId].colletralAmount,
+            proposalList[proposalId].totalMilestones,
+            proposalList[proposalId].status,
+            proposalList[proposalId].totalVotes,
+            proposalList[proposalId].proposer
         );
     }
 
@@ -124,134 +111,113 @@ contract DaoSmartContract is OwnableUpgradeSafe, PausableUpgradeSafe {
         uint256 endTimestamp,
         uint256 colletralAmount,
         uint256 totalMilestones,
-        string calldata _proposalId
+        string calldata proposalId
     ) external whenNotPaused {
         require(
-            proposalList[_proposalId].proposer == address(0),
+            proposalList[proposalId].proposer == address(0),
             "Proposal already submitted"
         );
-        proposalList[_proposalId] = Proposal(
+        proposalList[proposalId] = Proposal(
             fundsRequested,
             0,
             endTimestamp,
             colletralAmount,
             totalMilestones,
             0,
-            0, //status
+            0, 
             0,
             msg.sender
         );
         emit ProposalSubmitted(
-            _proposalId,
-            proposalList[_proposalId].fundsRequested,
-            proposalList[_proposalId].initiationTimestamp,
-            proposalList[_proposalId].completionTimestamp,
-            proposalList[_proposalId].colletralAmount,
-            proposalList[_proposalId].totalMilestones,
-            proposalList[_proposalId].status,
-            proposalList[_proposalId].totalVotes,
-            proposalList[_proposalId].proposer
+            proposalId,
+            proposalList[proposalId].fundsRequested,
+            proposalList[proposalId].initiationTimestamp,
+            proposalList[proposalId].completionTimestamp,
+            proposalList[proposalId].colletralAmount,
+            proposalList[proposalId].totalMilestones,
+            proposalList[proposalId].status,
+            proposalList[proposalId].totalVotes,
+            proposalList[proposalId].proposer
         );
     }
 
-    // function stake(
-    //     string calldata _proposalId,
-    //     uint256 _altQuantity,
-    //     uint256 _days
-    // ) public whenNotPaused returns (uint256 rewardAmount) {
-    //     require(
-    //         votes[msg.sender][_proposalId] == false,
-    //         "already staked once for this proposal"
-    //     );
-    //     rewardAmount = DaoStakeContract(phnxStakingContractAddress).stakeALT(
-    //         _altQuantity,
-    //         _days
-    //     );
-    //     votes[msg.sender][_proposalId] = true;
-    //     proposalList[_proposalId].totalVotes = proposalList[_proposalId]
-    //         .totalVotes
-    //         .add(1);
-    //     emit Voted(_proposalId, _altQuantity, _days, rewardAmount);
-    // }
 
-    function withdrawCollateral(string calldata _proposalId)
+    function withdrawCollateral(string calldata proposalId)
         external
         whenNotPaused
     {
         require(
-            proposalList[_proposalId].status == uint256(Status.COMPLETED),
+            proposalList[proposalId].status == uint256(Status.COMPLETED),
             "Project status not completed"
         );
         IERC20(phnxContractAddress).transfer(
-            proposalList[_proposalId].proposer,
-            proposalList[_proposalId].colletralAmount
+            proposalList[proposalId].proposer,
+            proposalList[proposalId].colletralAmount
         );
-        collateralAmount=collateralAmount.sub(proposalList[_proposalId].colletralAmount);
-        //rewardAmount=IERC20(phnxContractAddress).balanceOf(address(this)).sub(collateralAmount);
-        
+        collateralAmount=collateralAmount.sub(proposalList[proposalId].colletralAmount);
+
         emit ColleteralWithdrawn(
-            _proposalId,
-            proposalList[_proposalId].colletralAmount,
+            proposalId,
+            proposalList[proposalId].colletralAmount,
             msg.sender
         );
     }
 
-    function updateProposalStatus(string calldata _proposalId, uint256 _status)
+    function updateProposalStatus(string calldata proposalId, uint256 status)
         external
         onlyOwners
     {
         require(
-            proposalList[_proposalId].proposer != address(0),
+            proposalList[proposalId].proposer != address(0),
             "proposal not submitted before"
         );
-        uint256 oldStatus = proposalList[_proposalId].status;
+        uint256 oldStatus = proposalList[proposalId].status;
         if (
-            _status == uint256(Status.UPVOTE) &&
-            proposalList[_proposalId].status == uint256(Status.PENDING)
+            status == uint256(Status.UPVOTE) &&
+            proposalList[proposalId].status == uint256(Status.PENDING)
         ) {
-            proposalList[_proposalId].status = uint256(Status.UPVOTE);
+            proposalList[proposalId].status = uint256(Status.UPVOTE);
             IERC20(phnxContractAddress).transferFrom(
-                proposalList[_proposalId].proposer,
+                proposalList[proposalId].proposer,
                 address(this),
-                proposalList[_proposalId].colletralAmount
+                proposalList[proposalId].colletralAmount
             );
-            collateralAmount=collateralAmount.add(proposalList[_proposalId].colletralAmount);
-            //rewardAmount=address(this).balance.sub(collateralAmount);
-            //
+            collateralAmount=collateralAmount.add(proposalList[proposalId].colletralAmount);
+
         }
 
         if (
-            _status == uint256(Status.VOTING) &&
-            proposalList[_proposalId].status == uint256(Status.UPVOTE)
+            status == uint256(Status.VOTING) &&
+            proposalList[proposalId].status == uint256(Status.UPVOTE)
         ) {
-            proposalList[_proposalId].status = uint256(Status.VOTING);
+            proposalList[proposalId].status = uint256(Status.VOTING);
         }
         if (
-            _status == uint256(Status.ACTIVE) &&
-            proposalList[_proposalId].status == uint256(Status.VOTING)
+            status == uint256(Status.ACTIVE) &&
+            proposalList[proposalId].status == uint256(Status.VOTING)
         ) {
-            proposalList[_proposalId].status = uint256(Status.ACTIVE);
-            proposalList[_proposalId].initiationTimestamp=block.timestamp;
+            proposalList[proposalId].status = uint256(Status.ACTIVE);
+            proposalList[proposalId].initiationTimestamp=block.timestamp;
         }
         if (
-            _status == uint256(Status.REJECTED) &&
-            proposalList[_proposalId].status == uint256(Status.PENDING)
+            status == uint256(Status.REJECTED) &&
+            proposalList[proposalId].status == uint256(Status.PENDING)
         ) {
-            proposalList[_proposalId].status = uint256(Status.REJECTED);
+            proposalList[proposalId].status = uint256(Status.REJECTED);
         }
 
         if (
-            _status == uint256(Status.COMPLETED) &&
-            proposalList[_proposalId].status == uint256(Status.ACTIVE)
+            status == uint256(Status.COMPLETED) &&
+            proposalList[proposalId].status == uint256(Status.ACTIVE)
         ) {
-            proposalList[_proposalId].status = uint256(Status.COMPLETED);
+            proposalList[proposalId].status = uint256(Status.COMPLETED);
         }
 
-        require(proposalList[_proposalId].status == _status, "Status updated");
+        require(proposalList[proposalId].status == status, "Status updated");
         emit ProposalStatusUpdated(
-            _proposalId,
+            proposalId,
             oldStatus,
-            proposalList[_proposalId].status
+            proposalList[proposalId].status
         );
     }
 
