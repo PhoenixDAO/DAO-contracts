e9bbae0962d6e1713000d528d7bd22ac0aa58d57
Danial Ansari <danialansari998@gmail.com>
2021-08-10 15:52:10 +0500

    Pushing Code for Mainnet

---
+++ contracts/utils/MultipleOwners.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
+
+
+
+contract MulitpleOwners is OwnableUpgradeSafe {
+
+    mapping(address => bool) public isOwner;
+    modifier onlyOwners(){
+        require(isOwner[_msgSender()], "Ownable: caller is not one of many owner");
+         _;
+    }
+
+    function addOwner(address newOwner) external virtual onlyOwner{
+        isOwner[newOwner] = true;
+        emit AddedSubOwner(newOwner);
+    }
+
+    function removeOwner(address _subOwner) external virtual onlyOwner{
+        isOwner[_subOwner] = false;
+        emit RemovedSubOwner( _subOwner);
+    }
+
+}
--- networks.js
+++ networks.js
@@ -15,7 +15,7 @@ module.exports = {
         // 0x609a8a9ef6fa45955886ea323a70ab0ded6b5a656214fea4680605557c1d88b8
         //0x5d4af089d553bd1211888e81a41cf421e5e8f3e7d8e685675bdfcc47d79a5ca3
         //0xc569f7f4e2ff5ce8c4c7cee9aa0afdc50d9f590bdb8a0d94686d1d0d97eaed01
-        "0x609a8a9ef6fa45955886ea323a70ab0ded6b5a656214fea4680605557c1d88b8", `https://rinkeby.infura.io/v3/e3c9ab0b78e946b486fa9c7b5f26c3c1`
+        "0x5d4af089d553bd1211888e81a41cf421e5e8f3e7d8e685675bdfcc47d79a5ca3", `https://rinkeby.infura.io/v3/e3c9ab0b78e946b486fa9c7b5f26c3c1`
       ),
       networkId: "*",
       gas: 10000000,
